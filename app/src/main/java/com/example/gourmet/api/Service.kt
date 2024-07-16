901330a3156102e5972d5292a0211dca03634317
ArD <ademkhina@list.ru>
2024-07-16 14:55:17 +0700

    Почти красиво, но не работает

---
+++ app/src/main/java/com/example/gourmet/api/Service.kt
@@ -0,0 +1,27 @@
+package com.example.gourmet.api
+
+//import retrofit2.Response
+//import retrofit2.http.Body
+//import retrofit2.http.DELETE
+//import retrofit2.http.GET
+//import retrofit2.http.POST
+//import retrofit2.http.PUT
+//import retrofit2.http.Path
+//
+//interface Service {
+//    @POST("api/recipe")
+//    suspend fun createRecipe()
+//
+//    @GET("api/recipe")
+//    suspend fun getRecipe()
+//
+//    @GET("api/recipe/{id}")
+//    suspend fun getRecipeById(@Path("id") id: Int)
+//
+//    @PUT("api/recipe/{id}")
+//    suspend fun updateRecipe(@Path("id") id: Int)
+//
+//    @DELETE("api/recipe/{id}")
+//    suspend fun deleteRecipe(@Path("id") id: Int)
+//
+//}
